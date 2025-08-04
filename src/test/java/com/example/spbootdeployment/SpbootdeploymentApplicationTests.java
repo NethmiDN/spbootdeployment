d60db095e6a80b7fd4342457efe8988913a051d8
Nethmi Diwyanga Nanayakkara <nethminanayakkara277@gmail.com>
2025-08-04 10:04:45 +0530

    init

--- src/test/java/com/example/spbootdeployment/SpbootdeploymentApplicationTests.java
+++ src/test/java/com/example/spbootdeployment/SpbootdeploymentApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpbootdeploymentApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
